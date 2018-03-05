d0561185fc97e18314f9dd6be61951758b98dfde
Octavian Gheorghiu <octavian.gheorghiu@yahoo.com>
2018-03-05 20:47:58 +0000

    Session 1
    Installed HoloLens Tools and made a spinning cube.

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			GetComponent<Animator> ().SetBool ("SpinCube", true);
+		} else {
+			GetComponent<Animator> ().SetBool ("SpinCube", false);
+		}
+	}
+}
